3969bed5cf5a584fdaf61628cfa8c277f8f87eeb
Joseph Mead <mead@bnl.gov>
2026-01-05 12:13:03 -0500

    First commit

---
+++ iocBoot/iocqfa/st.cmd
@@ -0,0 +1,30 @@
+#!../../bin/linux-x86_64/pdu
+
+< envPaths
+
+epicsEnvSet("IOCNAME", "QFAMux")
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/qfa.dbd"
+pdu_registerRecordDeviceDriver pdbbase
+
+
+var("PSCDebug", "5")
+
+# Listen on 0.0.0.0:1234  (pass zero for random port)
+# for messages coming from "device" localhost:5000
+
+createPSCUDP("pscudp1", "10.0.142.195", 5000, 1234)
+#createPSCUDP("pscudp2", "10.0.142.187", 5000, 1235)
+
+## Load record instances
+dbLoadRecords("db/qfa.db","P=$(IOCNAME), NO=1")
+#dbLoadRecords("db/qfa.db","P=$(IOCNAME), NO=2")
+
+
+
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
